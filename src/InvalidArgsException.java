7eba0e6f5f3142a93111e6be4dbf07d091628d8a
vecin2 <vecin2@gmail.com>
2017-01-14 15:29:28 +0000

    moving hiddent files to new structure

--- src/InvalidArgsException.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-public class InvalidArgsException extends Exception{
-
-	public InvalidArgsException(String exceptionMsg) {
-		super(exceptionMsg);
-	}
-
-}
--- src/InvalidConversionKey.java
+++
@@ -1,8 +0,0 @@
-package src;
-
-public class InvalidConversionKey extends Exception {
-
-	public InvalidConversionKey(String errorMsg) {
-		super(errorMsg);
-	}
-}
